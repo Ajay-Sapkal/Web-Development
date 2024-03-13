dc7574dbfb804f4631f7d98572d53e92fcaecdb0
Ajay-Sapkal <ajaysapkal1000@gmail.com>
2024-03-13 09:59:41 +0530

    commit in branch

--- #39 JavaScript Strings/index.js
+++ #39 JavaScript Strings/index.js
@@ -7,7 +7,16 @@ console.log(a[2])
 
 console.log(a.length)
 
-let Name = "Ajay"
+let Name = "Vedant"
 let friend = "vedant"
-console.log("His name is " + Name + "and his friends name is " + friend);
-console.log('His name is ${Name} and his friends name is ${friend}')
+console.log("His name is " + Name + " and his friends name is " + friend);
+console.log(`His name is ${Name} and his friends name is ${friend}`)
+
+let b = "Ajay"
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+
+console.log(b.replace("ar", "55"))
+console.log(b.concat(Name))
